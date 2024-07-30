36591a4600a32f78f70dcc87447d9505a172c48b
pla1n <danilkuisis@gmail.com>
2024-07-30 18:45:16 +0300

    asd

--- src/data.js
+++ src/data.js
@@ -1,18 +1,18 @@
 const users = [
     {
-        img: '../imgs/sasha.png',
+        img: 'https://img-cdn.hltv.org/playerbodyshot/4NNsZrSGWLr9mZNt0Pe3KS.png?ixlib=java-2.1.0&w=400&s=4484cc99087121a6f9877d3742717444',
         name: 'Sasha',
         age: 26,
         text: 'Oleksandr Olehovych Kostyliev, better known as s1mple, is a Ukrainian professional Counter-Strike 2 player for Natus Vincere. He is considered to be one of the best players in Counter-Strike history.',
     },
     {
-        img: '../imgs/ilya.png',
+        img: 'https://settings.pro/wp-content/uploads/2021/03/player-picture-m0nesy.png?ver=1708190364',
         name: 'Ilya',
         age: 19,
         text: "Ilya “m0NESY” Osipov is a Russian professional Counter-Strike 2 player. He is currently playing for G2 Esports as an AWPer. Born on May 1, 2005, he is considered one of the game's greatest talents",
     },
     {
-        img: '../imgs/igor.png',
+        img: 'https://img-cdn.hltv.org/playerbodyshot/-_UTOwRq10TZ6Fzs_Gp_m1.png?ixlib=java-2.1.0&w=400&s=07067310960a920155ce725b27969258',
         name: 'Igor',
         age: 19,
         text: 'Ihor "w0nderful" Zhdanov (born December 14, 2004) is a Ukrainian professional Counter-Strike 2 player and a former Counter-Strike: Global Offensive player.',
